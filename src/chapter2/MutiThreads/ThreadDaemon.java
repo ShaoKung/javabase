b8c745be5df4e9b8e9fcc31856a587eef33a6971
shaoqing@asiainfo.com <1qaz2wsx>
2019-08-02 10:32:44 +0800

    Java并发编程从入门到精通

---
+++ src/chapter2/MutiThreads/ThreadDaemon.java
@@ -0,0 +1,14 @@
+package chapter2.MutiThreads;
+
+public class ThreadDaemon extends Thread{
+    public void run(){
+        for (int i=0;i<5;i++) {
+            System.out.println("线程ThreadDaemon第"+i+"次执行");
+            try {
+                Thread.sleep(7);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
